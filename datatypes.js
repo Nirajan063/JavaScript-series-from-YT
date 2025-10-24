46bd022f3fe535c57d81bf4005cc94154293fc79
Nirajan063 <nirajansingh632@gmail.com>
2025-10-24 16:44:31 +0545

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+let score = 33
+
+console.log(score);
+console.log(typeof score);
+
+let number = String(score)
+console.log(number);
+console.log(typeof number);
+
+let isLoggedIn = 1
+let loggedIn = Boolean(isLoggedIn)
+console.log(typeof loggedIn)
+console.log(loggedIn)
